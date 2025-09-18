e9df7e6119f5a198cffda5802e46431d6b84a0d8
Akhil-kintali <61041605+Akhil-kintali@users.noreply.github.com>
2025-09-18 14:38:35 +0530

    Update list-users.sh

--- list-users.sh
+++ list-users.sh
@@ -48,9 +48,10 @@ function list_users_with_read_access {
 
 function helper {
 expected_cmd_args=2
-if [$# -ne $expected_cmd_args]; then 
+if [ $# -ne $expected_cmd_args ]; then 
 echo "please execute the script with required cmd args"
-
+exit 1
+fi
 }
 
 # Main script
